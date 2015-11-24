d500f6abf83a013f1682750a9a4f950e1d561dff
Peter Hurford <peter@peterhurford.com>
2015-11-23 21:46:24 -0500

    rm early return

--- routes.R
+++ routes.R
@@ -8,7 +8,6 @@ routes <- Ramd::define("helpers/error", function(error) {
   "/process" = Ramd::define("helpers/run_code", function(run_code) {
       force(run_code)
       function(params, query) {
-        return(params)
         params <- as.list(params)
         required <- c('body')
         for (name in required) {
